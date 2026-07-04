d0c6ac7b94bd5c7067fbc58793d27f174bda6037
fa3677011-design <fa3677011@gmail.com>
2026-07-04 20:47:01 +0700

    Fix Vercel config and entry point for Laravel 12

--- api/index.php
+++ api/index.php
@@ -1,3 +1,23 @@
 <?php
 
-require __DIR__ . '/../public/index.php';
+use Illuminate\Foundation\Application;
+use Illuminate\Http\Request;
+
+define('LARAVEL_START', microtime(true));
+
+// Allow Vercel to locate storage & bootstrap from project root
+$root = __DIR__ . '/..';
+
+// Determine if the application is in maintenance mode...
+if (file_exists($maintenance = $root . '/storage/framework/maintenance.php')) {
+    require $maintenance;
+}
+
+// Register the Composer autoloader...
+require $root . '/vendor/autoload.php';
+
+// Bootstrap Laravel and handle the request...
+/** @var Application $app */
+$app = require_once $root . '/bootstrap/app.php';
+
+$app->handleRequest(Request::capture());
